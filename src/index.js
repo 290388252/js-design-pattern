cb48f8772866bf1aebb58d8ed629f8165fb54399
YChao <290388252@qq.com>
2018-10-23 18:02:34 +0800

    [First] first commit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
-alert('hello world');
+class Person {
+    constructor(name) {
+        this.name = name;
+    }
+    getName() {
+        return this.name;
+    }
+}
+class Student extends Person {
+    constructor(name, age) {
+        super(name)
+        this.age = age;
+    }
+    getAge() {
+        return this.age;
+    }
+}
+let p = new Student('yanchao1', 12);
+alert(p.getName());
+alert(p.getAge());
